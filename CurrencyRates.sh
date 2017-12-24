ffa34a5f738ca77adfd3bcbdba80b0b4bcb4f7c1
Jammicus <jamesmaxwalter@gmail.com>
2017-12-24 15:22:20 +0000

    Added basic support for crypto currencies by using crypto or normal command line arguments. Updated README. Automatic update currencies to uppercase to prevent API's from saying invalid currencies

--- CurrencyRates.sh
+++ CurrencyRates.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
+#************************************************#
+#               CurrencyRates.sh                 #
+#             written by James Walter            s#
+#               24,December 2017                 #
+#                                                #
+#              Gathers Curreny rates             #
+#************************************************#
 # Takes a currency and returns the exchange rate to it.
 # Uses JQ
 # https://stedolan.github.io/jq/ 
+
 jqAssert=`command -v jq`
 
 if [[ $jqAssert == "" ]]; then
     echo "You need to install the JQ package to run this script"
     exit 1
 fi
-echo "Please enter the Currency you want rates on"
 
-read currency
+function genericCurrency {
 
-curl -X GET https://api.fixer.io/latest?base=$currency | jq 
+    curl -s -X GET "https://api.fixer.io/latest?base=$currency" | jq 
 
-exit 0
+    exit 0
+}
+
+function cryptoCurrency {
+    curl -s  -X GET "https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currency}&tsyms=BTC,USD,EUR" | jq
+
+    exit 0
+}
+
+if [[ $1 == "crypto" ]]
+then
+    currency=$(echo $2 | tr /a-z/ /A-Z/)
+    cryptoCurrency
+elif [[ $1 == "normal" ]]
+then
+    currency=$(echo $2 | tr /a-z/ /A-Z/)
+    genericCurrency
+else
+    echo "bad input, please use crypto <currency> or normal <currency>"        
+    exit 1
+fi
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CurrencyRates
-Basic bash script to get currency rates
+Basic bash script to get crypto or normal currency rates.
 
 # Dependancies.
 
@@ -7,10 +7,14 @@ This script uses the JQ command to show the rates in a clear format
 
 If you do not have JQ, you can get it here: https://stedolan.github.io/jq/
 
+You also need internet access so we can poll the desired API to get the rates.
+
 
 # Usages
 
-Call the script and enter the currency based on its 3 character format
+To get the rates of a normal currency, use 
+`normal <currency>`
+To get the rates of a crypto currency, use
+`crypto <currency>`
 
-Eg: British Pound = GBP, US dollars = USD
-
+In both cases, the currency should be the 3 letter  abbreviation of the currency. For example, British Pounds would be GBP
